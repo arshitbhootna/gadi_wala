b8304af1066c044f55da17f3f78d2249345101b1
Arshit Bhootna <arshitbhootna9414@gmail.com>
2024-12-31 11:46:51 +0530

    d

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
 
+connectToDb();
 app.use(cors());
 app.use('/',(req,res)=>{
     res.send("Hello world")
