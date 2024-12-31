8affdb5280e1001f7a6c4008aed7b2b489f8d540
Arshit Bhootna <arshitbhootna9414@gmail.com>
2024-12-31 11:31:56 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use('/',(req,res)=>{
+    res.send("Hello world")
+})
