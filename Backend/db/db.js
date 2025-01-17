51949e814ca40e0e05f32b3a52511a6990caae07
Arshit Bhootna <arshitbhootna9414@gmail.com>
2025-01-18 01:42:14 +0530

    final commit

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectToDb = () => {
-    mongoose.connect(process.env.DB_CONNECT)
-        .then(() => console.log("Connected to DB"))
-        .catch(err => console.log(err));
-};
+
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT
+    ).then(() => {
+        console.log('Connected to DB');
+    }).catch(err => console.log(err));
+}
+
 
 module.exports = connectToDb;
